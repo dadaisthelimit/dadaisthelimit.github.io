042e0cc6cf44fe3fb2a82e548f4246d2499ab38b
dadaisthelimit <dadaisthelimit@gmail.com>
2023-05-05 19:01:25 +0800

    almost final version

--- trigger.js
+++ trigger.js
@@ -24,8 +24,38 @@ $(document).ready(function () {
     });
   });
 
-document.getElementById("submit-btn").addEventListener("click", function(event){
-    event.preventDefault();
-    alert("Thank you for your message, we'll get back to you withing 24 hours!");
+  $(document).ready(function () {
+    // Add event listener to submit button
+    $("#submit-btn").click(function (event) {
+      event.preventDefault(); // prevent the default form submission behavior
+      alert("Thank you for your message, we'll get back to you within 24 hours!"); // display confirmation message
+      // Reset form
+      $("#contact-form")[0].reset();
+    });
+  
+    // Add animation to elements in viewport
+    const animationTrigger = document.querySelector('.animation-trigger');
+    function isElementInViewport(el) {
+      const rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+    function addAnimationClass() {
+      if (isElementInViewport(animationTrigger)) {
+        animationTrigger.classList.add('animate__fadeInLeft');
+      }
+    }
+    window.addEventListener('scroll', addAnimationClass);
+    
+    // Toggle mobile navbar
+    $(".navbar-toggler").click(function () {
+      $(".navbar-collapse").slideToggle(200);
+    });
   });
   
+  
+ 
