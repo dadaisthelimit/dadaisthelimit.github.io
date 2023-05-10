f5d5d32616145433d73256cbcbeac2d5fcc10f3e
dadaisthelimit <dadaisthelimit@gmail.com>
2023-05-10 18:57:39 +0800

    test submit button

--- trigger.js
+++ trigger.js
@@ -18,44 +18,30 @@ function addAnimationClass() {
 
 window.addEventListener('scroll', addAnimationClass);
 
-$(document).ready(function () {
-    $(".navbar-toggler").click(function () {
-      $(".navbar-collapse").slideToggle(200);
-    });
+$(document).ready(function() {
+  // Toggle mobile navbar
+  $(".navbar-toggler").click(function() {
+    $(".navbar-collapse").slideToggle(200);
   });
-
-  $(document).ready(function () {
-    // Add event listener to submit button
-    $("#submit-btn").click(function (event) {
-      event.preventDefault(); // prevent the default form submission behavior
-      alert("Thank you for your message, we'll get back to you within 24 hours!"); // display confirmation message
-      // Reset form
-      $("#contact-form")[0].reset();
-    });
   
-    // Add animation to elements in viewport
-    const animationTrigger = document.querySelector('.animation-trigger');
-    function isElementInViewport(el) {
-      const rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-    }
-    function addAnimationClass() {
-      if (isElementInViewport(animationTrigger)) {
-        animationTrigger.classList.add('animate__fadeInLeft');
-      }
-    }
-    window.addEventListener('scroll', addAnimationClass);
+  // Add submit button function
+  function confirmSubmit(event) {
+    event.preventDefault(); // prevent the default form submission behavior
     
-    // Toggle mobile navbar
-    $(".navbar-toggler").click(function () {
-      $(".navbar-collapse").slideToggle(200);
-    });
-  });
-  
-  
- 
+    var confirmation = confirm("Are you sure you want to submit the form?");
+    if (confirmation) {
+      // submit the form
+      document.getElementById("myForm").submit();
+      
+      // reset the form
+      document.getElementById("myForm").reset();
+      
+      // display a success message
+      alert("Form submitted successfully!");
+      
+      // return to the top of the page
+      window.scrollTo(0, 0);
+    }
+    return confirmation;
+  }
+});
